55c4db9f78824a24e81bc07bf6f7a3f8cc8047b0
KIRBY-PARK <pkirby0426@gmail.com>
2025-01-16 17:25:21 +0900

    init: Create a project Project creation completed.

---
+++ src/main/java/com/example/kirbyVerse/KirbyVerseApplication.java
@@ -0,0 +1,13 @@
+package com.example.kirbyVerse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KirbyVerseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KirbyVerseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/kirbyVerse/KirbyVerseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.kirbyVerse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KirbyVerseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
